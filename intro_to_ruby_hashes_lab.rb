17e90788f1beff2c6abc0e2f057dd08ec6e95153
Asif Kamal <asif_kamal@hotmail.com>
2019-12-02 20:11:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,8 @@ def monopoly_with_third_tier
    :four_pieces_owned => 200},
    
    :names => {
-     :reading_railroad => {"mortgage_value" => ""}, 
-     :pennsylvania_railroad => {},
+     :reading_railroad => {"mortgage_value" => "800"}, 
+     :pennsylvania_railroad => {"mortgage_value" => "800"},
      :b_and_o_railroad => {}, 
      :shortline_railroad => {}
      
