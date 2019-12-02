8f51b774602ca8d57efebb9ac10c4ed2123463f6
Asif Kamal <asif_kamal@hotmail.com>
2019-12-02 19:39:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash = {:railroads => {:pieces => 4}}
-  hash = {:rent_in_dollars 
+  hash = {:railroads => {:pieces => 4}, :rent_in_dollars => {}}
+  
 end
 
 def monopoly_with_fourth_tier
